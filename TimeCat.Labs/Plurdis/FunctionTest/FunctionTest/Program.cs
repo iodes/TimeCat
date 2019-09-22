50132046a46e6f076dd65a114a8fe8427d714aea
Plurdis <uutak2000@naver.com>
2019-09-23 03:28:37 +0900

    TimeCat.Labs 하위에 FunctionTest 프로젝트 추가

---
+++ TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
