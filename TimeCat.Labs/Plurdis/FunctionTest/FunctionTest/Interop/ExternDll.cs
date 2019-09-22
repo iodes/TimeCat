f15d0f0ba2c2bf4e521b7b344f8060947f9ebcd4
Plurdis <uutak2000@naver.com>
2019-09-23 04:10:04 +0900

    User32 API 추가 (EnumWindows, GetWindowLog, GetLastInputInfo)

---
+++ TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Interop/ExternDll.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionTest.Interop
+{
+    static class ExternDll
+    {
+        public const string User32 = "user32.dll";
+    }
+}
---
+++ TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Interop/User32.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace FunctionTest.Interop
+{
+    class User32
+    {
+        #region [  API Declaration  ]
+
+        public delegate bool EnumWindowsProc(IntPtr hWnd, int lParam);
+
+        [DllImport(ExternDll.User32)]
+        public static extern bool EnumWindows(EnumWindowsProc enumFunc, int lParam);
+
+        [DllImport(ExternDll.User32)]
+        public static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport(ExternDll.User32)]
+        static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        #endregion
+
+        #region [  Struct Declaration  ]
+
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+
+        #endregion
+    }
+}
