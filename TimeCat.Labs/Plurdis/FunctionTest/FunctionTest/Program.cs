d1efde80648675297e932bb14ac2fac5cf7b65ce
Plurdis <uutak2000@naver.com>
2019-09-23 23:53:24 +0900

    Add FunctionTest Project

---
+++ TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using FunctionTest.Interop;
+
+namespace FunctionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
