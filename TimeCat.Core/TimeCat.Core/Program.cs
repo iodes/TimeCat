bfdf04c60ca6a5ee473fa9056109405a19db441b
최진용 <godhoop@nate.com>
2019-09-26 22:10:11 +0900

    Add Storage Environment

---
+++ TimeCat.Core/TimeCat.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeCat.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.Database);
+        }
+    }
+}
