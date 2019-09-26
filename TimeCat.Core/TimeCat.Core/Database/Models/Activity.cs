8e67d025c29c316aeaf0b77e4a73f77a93f347e6
최진용 <godhoop@nate.com>
2019-09-27 01:01:30 +0900

    Impl Database models

---
+++ TimeCat.Core/TimeCat.Core/Database/Models/Activity.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+namespace TimeCat.Core.Database.Models
+{
+    [Table("activities")]
+    public class Activity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+    }
+}
---
+++ TimeCat.Core/TimeCat.Core/Database/TimeCatDB.cs
@@ -0,0 +1,46 @@
+﻿using SQLite;
+using SQLiteNetExtensionsAsync.Extensions;
+using System.Collections.Generic;
+using System.Reflection;
+using TimeCat.Core.Database.Models;
+
+namespace TimeCat.Core.Database
+{
+    sealed class TimeCatDB : SQLiteDatabase
+    {
+        static readonly PropertyInfo _applicationCategoryProperty;
+
+        static TimeCatDB()
+        {
+            _applicationCategoryProperty = typeof(Application).GetRuntimeProperty(nameof(Application.Category));
+        }
+
+        protected override void OnInitialize(SQLiteConnection connection)
+        {
+            connection.CreateTable<Category>();
+            connection.CreateTable<Application>();
+            //connection.CreateTable<Activity>();
+        }
+
+        public IAsyncEnumerable<Category> GetCategories() => TableAsync<Category>();
+
+        public async IAsyncEnumerable<Application> GetApplications()
+        {
+            await foreach (var application in TableAsync<Application>())
+            {
+                await ReadOperations.GetChildAsync(Connection, application, _applicationCategoryProperty);
+                yield return application;
+            }
+        }
+
+        #region Singleton
+        static TimeCatDB _instance;
+
+        public static TimeCatDB Instance => _instance ?? (_instance = new TimeCatDB());
+
+        private TimeCatDB()
+        {
+        }
+        #endregion
+    }
+}
