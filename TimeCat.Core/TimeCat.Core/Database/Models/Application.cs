b3e541c14990b785af730743322ffd5058baeb11
최진용 <godhoop@nate.com>
2019-09-27 01:01:30 +0900

    Impl Database models

---
+++ TimeCat.Core/TimeCat.Core/Database/Models/Application.cs
@@ -0,0 +1,38 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace TimeCat.Core.Database.Models
+{
+    [Table("applications")]
+    public class Application
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets the display name of the application
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the full path of the application
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Gets the display icon path of the application
+        /// </summary>
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// Gets the display version of the application
+        /// </summary>
+        public string Version { get; set; }
+        
+        [ForeignKey(typeof(Category))]
+        public int CategoryId { get; set; }
+
+        [OneToOne]
+        public Category Category { get; set; }
+    }
+}
---
+++ TimeCat.Core/TimeCat.Core/Database/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace TimeCat.Core.Database.Models
+{
+    [Table("categories")]
+    public class Category
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets the name of category
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
