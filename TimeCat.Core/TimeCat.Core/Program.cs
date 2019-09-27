4c59d6c88a659e8c36411ad264a9657eeb1bbe5a
Kevin <iodesme@gmail.com>
2019-09-28 05:16:20 +0900

    Setup gRPC

--- TimeCat.Core/TimeCat.Core/Program.cs
+++ TimeCat.Core/TimeCat.Core/Program.cs
@@ -1,64 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using TimeCat.Core.Commons;
-using TimeCat.Core.Database;
-using TimeCat.Core.Database.Models;
+﻿using Grpc.Core;
+using System;
+using System.Threading;
+using TimeCat.Core.Services;
+using TimeCat.Proto;
 
 namespace TimeCat.Core
 {
     class Program
     {
-        static async Task Main(string[] args)
+        const string host = "localhost";
+        const int port = 37013;
+
+        static Server _server;
+        static AutoResetEvent _autoResetEvent;
+
+        static void Main(string[] args)
         {
-            await TimeCatDB.Instance.Initialize(Environment.Database);
+            _autoResetEvent = new AutoResetEvent(false);
 
-            await TimeCatDB.Instance.DeleteAllAsync<Category>();
-            await TimeCatDB.Instance.DeleteAllAsync<Application>();
-            await TimeCatDB.Instance.DeleteAllAsync<Activity>();
-
-            var categories = Enumerable.Range(0, 100)
-                .Select(i => new Category() { Name = $"Category {i}" });
-
-            await TimeCatDB.Instance.InsertRangeAsync(categories);
-
-            var applications = new List<Application>();
-
-            await foreach (Category c in TimeCatDB.Instance.GetCategories())
+            _server = new Server
             {
-                applications.Add(new Application()
+                Services =
                 {
-                    Name = $"Test Application ({c.Id})",
-                    FullName = $"TimeCat.Core.Test.Application_{c.Id}",
-                    Version = $"1.0.0.{c.Id}",
-                    Icon = "C:\\icon.png",
-                    CategoryId = c.Id
-                });
-            }
-
-            await TimeCatDB.Instance.InsertRangeAsync(applications);
-
-            var activities = new List<Activity>();
-
-            await foreach (Application app in TimeCatDB.Instance.GetApplications())
-            {
-                Console.WriteLine($"App: {app.Id}, {app.Name}({app.Version})");
-
-                activities.Add(new Activity()
+                    CommonService.BindService(new CommonRpc())
+                },
+                Ports =
                 {
-                    ApplicationId = app.Id,
-                    Action = ActionType.Active,
-                    Time = DateTimeOffset.Now
-                });
-            }
+                    new ServerPort(host, port, null)
+                }
+            };
 
-            await TimeCatDB.Instance.InsertRangeAsync(activities);
+            _server.Start();
 
-            await foreach (Activity activity in TimeCatDB.Instance.GetActivities())
-            {
-                Console.WriteLine($"Activity: {activity.Id}, {activity.Action}, {activity.Time}");
-            }
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+
+            _autoResetEvent.WaitOne();
+        }
+
+        private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            _server.ShutdownAsync().Wait();
+            _autoResetEvent.Set();
         }
     }
 }
