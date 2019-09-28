bc7aee449c979d1b951ead69f21e3a830f02c491
Plurdis <uutak2000@naver.com>
2019-09-28 15:13:44 +0900

    Add SetWinEventHook API Sample

--- TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Program.cs
+++ TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Program.cs
@@ -1,19 +1,36 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using FunctionTest.Interop;
+﻿using FunctionTest.Interop;
+using System;
+using System.Threading;
 
 namespace FunctionTest
 {
     class Program
     {
+        static User32.WinEventDelegate dele = new User32.WinEventDelegate(WinEventProc);
+
         static void Main(string[] args)
         {
-            User32.GetOpenWindows()
-                .ToList()
-                .ForEach(i => Console.WriteLine(i.Value));
+            var thread = new Thread(() =>
+            {
+                IntPtr m_hhook = User32.SetWinEventHook(User32.eventSystemForeground,
+                    User32.eventSystemForeground,
+                    IntPtr.Zero,
+                    dele,
+                    0,
+                    0,
+                    User32.winEventOutOfContext);
+            });
+            thread.Start();
 
-            Console.ReadLine();
+            while (true)
+            {
+                Thread.Sleep(10);
+            }
+        }
+
+        private static void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            Console.WriteLine(User32.GetActiveWindowTitle());
         }
     }
 }
