3d73ba2d18e49450c1109478ca26541b85903167
김성백 <dumbuk12@naver.com>
2019-09-28 20:17:43 +0900

    Create Overview page and Divide Layout

---
+++ TimeCat.Labs/Baek2back/overview/src/Overview.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Row, Col } from "antd";
+import "antd/dist/antd.css";
+
+const Overview: React.FC = () => {
+  return (
+    <div style={styles.container}>
+      <Row>
+        <Col lg={12}>
+          <Row>
+            <Col xs={24}>
+              <Col xs={8} style={styles.colStyle}>
+                Total time
+              </Col>
+              <Col xs={8} style={styles.colStyle}>
+                Most active weekdays
+              </Col>
+              <Col xs={8} style={styles.colStyle}>
+                Most active hours
+              </Col>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={24}>
+              <Col xs={8} style={styles.colStyle}>
+                Productivity scroe
+              </Col>
+              <Col xs={8} style={styles.colStyle}>
+                Most producttive weekdays
+              </Col>
+              <Col xs={8} style={styles.colStyle}>
+                Most productive hours
+              </Col>
+            </Col>
+          </Row>
+        </Col>
+        <Col lg={12} style={styles.colStyle}>
+          Time per Project per Hour
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={12} style={styles.colStyle}>
+          Applications
+        </Col>
+        <Col lg={12} style={styles.colStyle}>
+          Projects & Tasks
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    backgroundColor: "#eeeeee"
+  },
+  colStyle: {
+    padding: 10,
+    display: "flex",
+    justifyContent: "center"
+  }
+};
+
+export default Overview;
