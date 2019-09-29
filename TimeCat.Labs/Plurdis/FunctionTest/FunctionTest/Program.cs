8454c5b25a30baaf9293994a7e93586eab45b3e5
Plurdis <uutak2000@naver.com>
2019-09-29 23:23:32 +0900

    Add Idle Check (over 5s) Test Code

--- TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Program.cs
+++ TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Program.cs
@@ -1,16 +1,20 @@
 ﻿using FunctionTest.Interop;
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace FunctionTest
 {
     class Program
     {
         static User32.WinEventDelegate dele = new User32.WinEventDelegate(WinEventProc);
+        static uint lastInputTick;
+        static int TargetMillseconds = 50;
 
         static void Main(string[] args)
         {
             var manualResetEvent = new ManualResetEvent(false);
+
             var thread = new Thread(() =>
             {
                 IntPtr m_hhook = User32.SetWinEventHook(User32.eventSystemForeground,
@@ -23,8 +27,38 @@ namespace FunctionTest
 
                 manualResetEvent.WaitOne();
             });
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
+
+            Task.Run(() =>
+            {
+                int i = 0;
+                while (true)
+                {
+                    uint tick = User32.GetLastInputTick();
+
+                    bool isMoved = lastInputTick != tick;
+                    lastInputTick = tick;
+
+                    if (i == TargetMillseconds)
+                        Console.WriteLine("IDLE Detected (Over 5s)");
+
+                    if (isMoved)
+                    {
+                        if (i >= TargetMillseconds)
+                            Console.WriteLine($"IDLE Time : {i * 100}ms");
+
+                        i = 0;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+
+                    Thread.Sleep(100);
+                }
+            });
+
+            //thread.SetApartmentState(ApartmentState.STA);
+            //thread.Start();
 
             Console.ReadLine();
             manualResetEvent.Set();
