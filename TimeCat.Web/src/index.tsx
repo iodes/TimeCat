435e519c11251e3241db9287aaa30f73025b34ca
TaeSang Cho <gsts007@gmail.com>
2019-10-01 11:29:42 +0900

    [Web] Use redux to react

--- TimeCat.Web/src/index.tsx
+++ TimeCat.Web/src/index.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const state = createStore(reducer)
+
+ReactDOM.render(
+    <Provider state={state}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+)
