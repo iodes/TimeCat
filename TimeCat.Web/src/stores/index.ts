b7fc2f6c1f77b1d6ebe229740870041ebeea1822
TaeSang Cho <gsts007@gmail.com>
2019-10-02 17:25:33 +0900

    [Web] Add tslint whitespace rule in import module

--- TimeCat.Web/src/stores/index.ts
+++ TimeCat.Web/src/stores/index.ts
@@ -1,6 +1,6 @@
 import { combineReducers, createStore } from 'redux'
 import filters from './filters/reducers'
-import {IFiltersState} from './filters/types'
+import { IFiltersState } from './filters/types'
 
 export interface IRootState {
     filters: IFiltersState
