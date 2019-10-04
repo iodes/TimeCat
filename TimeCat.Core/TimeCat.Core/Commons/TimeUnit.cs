ce4dc7033beb115f9d0b7e15528045cb67c54d39
최진용 <godhoop@nate.com>
2019-10-04 17:33:04 +0900

    Tweak enum

---
+++ TimeCat.Core/TimeCat.Core/Commons/TimeUnit.cs
@@ -0,0 +1,15 @@
+﻿namespace TimeCat.Core.Commons
+{
+    enum TimeUnit
+    {
+        None = 0,
+        Millisecond = 1,
+        Second = 2,
+        Minute = 3,
+        Hour = 4,
+        Day = 5,
+        Week = 6,
+        Month = 7,
+        Year = 8
+    }
+}
