0c2ec049feef2b27c55c01fe4f81865c386370a8
최진용 <godhoop@nate.com>
2019-10-04 18:06:04 +0900

    Add category service rpc

--- TimeCat.Core/TimeCat.Core/Services/CategoryService.cs
+++ TimeCat.Core/TimeCat.Core/Services/CategoryService.cs
@@ -1,9 +1,31 @@
-﻿using TimeCat.Proto.Services;
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+using TimeCat.Proto.Commons;
+using TimeCat.Proto.Services;
 
 namespace TimeCat.Core.Services
 {
     class CategoryService : RpcCategoryService.RpcCategoryServiceBase
     {
-        // TODO: override category rpc
+        public override Task<CategoryListResponse> GetCategories(Empty request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<CategoryCreateResponse> CreateCategory(CategoryCreateRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<Empty> UpdateCategory(CategoryUpdateRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<Empty> DeleteCategory(CategoryDeleteRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
