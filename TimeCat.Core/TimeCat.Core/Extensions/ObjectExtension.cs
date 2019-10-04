2b972db34d1d19eda439e1838928399891a05782
최진용 <godhoop@nate.com>
2019-10-04 17:40:31 +0900

    Add ObjectExtension

---
+++ TimeCat.Core/TimeCat.Core/Extensions/ObjectExtension.cs
@@ -0,0 +1,12 @@
+﻿using TimeCat.Core.Utilities;
+
+namespace TimeCat.Core.Extensions
+{
+    static class ObjectExtension
+    {
+        public static TDestination Map<TDestination>(this object from)
+        {
+            return MappingUtility.Map<TDestination>(from);
+        }
+    }
+}
