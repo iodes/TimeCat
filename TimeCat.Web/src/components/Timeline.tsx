6abb2cb9825d1c11936a812fc49ca546249c5f2c
TaeSang Cho <gsts007@gmail.com>
2019-10-05 17:16:15 +0900

    [Web] Change 4 space indent to 2 space
    - change tslint config
    - change all tsx, ts files
    - change webpack config file
    - change all json files

--- TimeCat.Web/src/components/Timeline.tsx
+++ TimeCat.Web/src/components/Timeline.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
 
 export class Timeline extends React.Component {
-	constructor(props: {}) {
-		super(props)
-	}
+  constructor(props: {}) {
+    super(props)
+  }
 
-	public render() {
-		return (
-			<h2>Hello, Timeline Component</h2>
-		)
-	}
+  public render() {
+    return (
+      <h2>Hello, Timeline Component</h2>
+    )
+  }
 }
 
 export default Timeline
