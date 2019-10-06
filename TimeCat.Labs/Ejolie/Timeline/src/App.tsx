4c570b9682d6eeaa2d8c154a391dd9f920dd6a86
Eunjeong Park <ejisvip@gmail.com>
2019-10-06 23:17:18 +0900

    Remove Nivo bar component and create my Timeline component

--- TimeCat.Labs/Ejolie/Timeline/src/App.tsx
+++ TimeCat.Labs/Ejolie/Timeline/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import MyResponsiveBar from './MyResponsiveBar';
+import Timeline from './Timeline';
 import { data } from './data';
 
 const App: React.FC = () => {
   return (
     <div style={{ height: 400 }}>
       <h1>Hello Timeline!</h1>
-      <MyResponsiveBar data={data} />
+      <Timeline data={data} />
     </div>
   );
 };
