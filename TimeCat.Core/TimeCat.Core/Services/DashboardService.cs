7375258c89bd3b2f5fc56f89418f263bc0781c30
LiteHell <litehell@litehell.info>
2019-10-08 23:53:56 +0900

    Implement DashboardService methods

--- TimeCat.Core/TimeCat.Core/Services/DashboardService.cs
+++ TimeCat.Core/TimeCat.Core/Services/DashboardService.cs
@@ -1,6 +1,11 @@
 ﻿using Grpc.Core;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using TimeCat.Core.Commons;
+using TimeCat.Core.Extensions;
+using TimeCat.Core.Database;
+using TimeCat.Core.Database.Models;
 using TimeCat.Proto.Commons;
 using TimeCat.Proto.Services;
 
@@ -8,14 +13,78 @@ namespace TimeCat.Core.Services
 {
     class DashboardService : RpcDashboardService.RpcDashboardServiceBase
     {
-        public override Task<TotalTimeResponse> GetTotalTime(TotalTimeRequest request, ServerCallContext context)
+        private readonly TimeCatDB _db = TimeCatDB.Instance;
+        const int activeMillis = 5000;
+        public override async Task<TotalTimeResponse> GetTotalTime(TotalTimeRequest request, ServerCallContext context)
         {
-            throw new NotImplementedException();
+            long totalTime = 0, activeSince = -1;
+            await foreach(Activity activity in _db.GetActivities())
+            {
+                if (activity.Time < request.Range.Start.ToDateTime() || activity.Time > request.Range.End.ToDateTime())
+                    continue;
+                if (activity.Action == ActionType.Active)
+                {
+                    long activityTicks = activity.Time.Ticks;
+                    if (activeSince == -1)
+                    {
+                        activeSince = activityTicks;
+                    }
+                    else
+                    {
+                        long passedTime = (activityTicks - activeSince);
+                        if (passedTime > activeMillis * 10000)
+                        {
+                            activeSince = -1;
+                        }
+                        else
+                        {
+                            totalTime += passedTime;
+                            activeSince = activityTicks;
+                        }
+                    }
+                }
+            }
+
+            return new TotalTimeResponse()
+            {
+                TotalTime = Google.Protobuf.WellKnownTypes.Duration.FromTimeSpan(new TimeSpan(totalTime))
+            };
         }
 
-        public override Task GetApplications(ApplicationRequest request, IServerStreamWriter<ApplicationResponse> responseStream, ServerCallContext context)
+        public override async Task GetApplications(ApplicationRequest request, IServerStreamWriter<ApplicationResponse> responseStream, ServerCallContext context)
         {
-            throw new NotImplementedException();
+            long totalTime = 0, activeSince = -1;
+            Application application = null;
+            await foreach (Activity activity in _db.GetActivities())
+            {
+                if (activity.Time < request.Range.Start.ToDateTime() || activity.Time > request.Range.End.ToDateTime())
+                    continue;
+                switch (activity.Action)
+                {
+                    case ActionType.Focus:
+                        application = await _db.GetAsync<Application>(activity.ApplicationId);
+                        activeSince = activity.Time.Ticks;
+                        break;
+                    case ActionType.Active:
+                        long activityTicks = activity.Time.Ticks;
+                        totalTime += (activityTicks - activeSince);
+                        activeSince = activityTicks;
+                        break;
+                    case ActionType.Blur:
+                        ApplicationResponse response = new ApplicationResponse()
+                        {
+                            Application = application.ToRpc(),
+                            TotalTime = Google.Protobuf.WellKnownTypes.Duration.FromTimeSpan(new TimeSpan(totalTime))
+                        };
+                        await responseStream.WriteAsync(response);
+                        activeSince = -1;
+                        totalTime = 0;
+                        application = null;
+                        break;
+                    default:
+                        continue;
+                }
+            }
         }
     }
 }
