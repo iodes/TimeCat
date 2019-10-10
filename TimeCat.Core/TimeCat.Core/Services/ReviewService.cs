ae9df46e1421e0126a44deb85a747e023caf4ef8
LiteHell <litehell@litehell.info>
2019-10-10 14:18:59 +0900

    Use Idle ActionType In ReviewSerivce

--- TimeCat.Core/TimeCat.Core/Services/ReviewService.cs
+++ TimeCat.Core/TimeCat.Core/Services/ReviewService.cs
@@ -1,10 +1,13 @@
-﻿using Grpc.Core;
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TimeCat.Core.Commons;
 using TimeCat.Core.Database;
 using TimeCat.Core.Database.Models;
 using TimeCat.Core.Extensions;
+using TimeCat.Proto.Commons;
 using TimeCat.Proto.Services;
 
 namespace TimeCat.Core.Services
@@ -12,59 +15,35 @@ namespace TimeCat.Core.Services
     class ReviewService : RpcReviewService.RpcReviewServiceBase
     {
         private readonly TimeCatDB _db = TimeCatDB.Instance;
-        const int activeMillis = 5000;
+
         public override async Task GetTimeline(TimelineRequest request, IServerStreamWriter<TimelineResponse> responseStream, ServerCallContext context)
         {
-            long activeSince = -1, activeUntil = -1;
-            Application application = null;
-            TimelineResponse response;
+            Dictionary<int, TimestampRange> timestampRanges = new Dictionary<int, TimestampRange>();
             await foreach (Activity activity in _db.GetActivities())
             {
                 if (activity.Time < request.Range.Start.ToDateTime() || activity.Time > request.Range.End.ToDateTime())
                     continue;
                 switch (activity.Action)
                 {
-                    case ActionType.Focus:
-                        application = await _db.GetAsync<Application>(activity.ApplicationId);
-                        activeSince = activity.Time.Ticks;
-                        break;
                     case ActionType.Active:
-                        long activityTicks = activity.Time.Ticks;
-                        long passedTime = activityTicks - activeSince;
-                        if (passedTime < activeMillis * 10000)
+                        if (!timestampRanges.ContainsKey(activity.ApplicationId))
                         {
-                            activeUntil = activityTicks;
-                        } else
-                        {
-                            response = new TimelineResponse()
-                            {
-                                Application = application.ToRpc(),
-                                Range = new Proto.Commons.TimestampRange() { 
-                                    Start = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(new DateTime(activeSince)),
-                                    End = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(new DateTime(activeUntil))
-                                }
-                            };
-                            await responseStream.WriteAsync(response);
-                            activeSince = -1;
-                            activeUntil = -1;
-                            application = null;
-                            break;
+                            timestampRanges.Add(activity.ApplicationId, new TimestampRange() { Start = Timestamp.FromDateTimeOffset(activity.Time) });
                         }
                         break;
-                    case ActionType.Blur:
-                        response = new TimelineResponse()
+                    case ActionType.Idle:
+                        if (timestampRanges.ContainsKey(activity.ApplicationId))
                         {
-                            Application = application.ToRpc(),
-                            Range = new Proto.Commons.TimestampRange()
+                            timestampRanges[activity.ApplicationId].End = Timestamp.FromDateTimeOffset(activity.Time);
+                            Application application = await _db.GetAsync<Application>(activity.ApplicationId);
+                            TimelineResponse response = new TimelineResponse()
                             {
-                                Start = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(new DateTime(activeSince)),
-                                End = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(new DateTime(activeUntil))
-                            }
-                        };
-                        await responseStream.WriteAsync(response);
-                        activeSince = -1;
-                        activeUntil = -1;
-                        application = null;
+                                Application = application.ToRpc(),
+                                Range = timestampRanges[activity.ApplicationId]
+                            };
+                            await responseStream.WriteAsync(response);
+                            timestampRanges.Remove(activity.ApplicationId);
+                        }
                         break;
                     default:
                         continue;
