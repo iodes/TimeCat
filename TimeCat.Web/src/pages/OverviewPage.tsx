47fb9a7987d42e3d52883554d173baf554aa92c6
김성백 <dumbuk12@naver.com>
2019-10-10 17:54:21 +0900

    Change from Class Component to functional Component and Apply Styled-Components

--- TimeCat.Web/src/pages/OverviewPage.tsx
+++ TimeCat.Web/src/pages/OverviewPage.tsx
@@ -1,15 +1,64 @@
+import { Col, Row } from 'antd'
+import 'antd/dist/antd.css'
 import * as React from 'react'
+import styled from 'styled-components'
 
-export class OverviewPage extends React.Component {
-  constructor(props: {}) {
-    super(props)
-  }
+const Container = styled('div')`
+  background-color: #eeeeee;
+`
 
-  public render() {
-    return (
-      <h1>Hello, Overview Page</h1>
-    )
-  }
+const Content = styled(Col)`
+  padding: 10;
+  display: flex;
+  justify-content: center;
+`
+
+const OverviewPage: React.FC = () => {
+  return (
+    <Container>
+      <Row>
+        <Col lg={12}>
+          <Row>
+            <Col xs={24}>
+              <Content xs={8}>
+                TotalTime
+              </Content>
+              <Content xs={8}>
+                ActiveWeekdays
+              </Content>
+              <Content xs={8}>
+                ActiveHours
+              </Content>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={24}>
+              <Content xs={8}>
+                Score
+              </Content>
+              <Content xs={8}>
+                ProductiveWeekday
+              </Content>
+              <Content xs={8}>
+                ProductiveHours
+              </Content>
+            </Col>
+          </Row>
+        </Col>
+        <Content lg={12}>
+          Stackedbar
+        </Content>
+      </Row>
+      <Row>
+        <Content lg={12}>
+          Applications
+        </Content>
+        <Content lg={12}>
+          Projects
+        </Content>
+      </Row>
+    </Container>
+  )
 }
 
 export default OverviewPage
