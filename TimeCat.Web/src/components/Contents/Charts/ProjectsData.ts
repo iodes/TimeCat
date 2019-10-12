e8599ecd8ea2a1c4868a9e4a1e0d83d8b9103c5b
김성백 <dumbuk12@naver.com>
2019-10-12 19:07:03 +0900

    Add dummy data for each component

---
+++ TimeCat.Web/src/components/Contents/Charts/ProjectsData.ts
@@ -0,0 +1,92 @@
+export const ProjectsData = [
+  {
+    id: 'make',
+    label: 'make',
+    value: 339,
+  },
+  {
+    id: 'python',
+    label: 'python',
+    value: 519,
+  },
+  {
+    id: 'c',
+    label: 'c',
+    value: 328,
+  },
+  {
+    id: 'hack',
+    label: 'hack',
+    value: 595,
+  },
+  {
+    id: 'php',
+    label: 'php',
+    value: 277,
+  },
+  {
+    id: 'haskell',
+    label: 'haskell',
+    value: 539,
+  },
+  {
+    id: 'go',
+    label: 'go',
+    value: 315,
+  },
+  {
+    id: 'ruby',
+    label: 'ruby',
+    value: 325,
+  },
+  {
+    id: 'java',
+    label: 'java',
+    value: 163,
+  },
+  {
+    id: 'javascript',
+    label: 'javascript',
+    value: 237,
+  },
+  {
+    id: 'scala',
+    label: 'scala',
+    value: 46,
+  },
+  {
+    id: 'elixir',
+    label: 'elixir',
+    value: 9,
+  },
+  {
+    id: 'erlang',
+    label: 'erlang',
+    value: 475,
+  },
+  {
+    id: 'css',
+    label: 'css',
+    value: 228,
+  },
+  {
+    id: 'sass',
+    label: 'sass',
+    value: 442,
+  },
+  {
+    id: 'stylus',
+    label: 'stylus',
+    value: 403,
+  },
+  {
+    id: 'rust',
+    label: 'rust',
+    value: 25,
+  },
+  {
+    id: 'lisp',
+    label: 'lisp',
+    value: 65,
+  },
+]
