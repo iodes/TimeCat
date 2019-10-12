69dfbd908d9f1151ad112e82bd90bcca56706d32
Plurdis <uutak2000@naver.com>
2019-10-12 18:50:45 +0900

    Add WindowsApplication Class (Implements from IApplication)

---
+++ TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Driver/Windows/WindowsApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace FunctionTest.Driver.Windows
+{
+    public class WindowsApplication : IApplication
+    {
+        public int ProcessId { get; set; }
+
+        public string Caption { get; set; }
+    }
+}
