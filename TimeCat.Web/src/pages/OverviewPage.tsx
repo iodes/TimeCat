6bc7f877aaf58e6835594a2e78e952601fa3ad63
김성백 <dumbuk12@naver.com>
2019-10-12 19:08:40 +0900

    Apply Styled-Components to All Components

--- TimeCat.Web/src/pages/OverviewPage.tsx
+++ TimeCat.Web/src/pages/OverviewPage.tsx
@@ -2,14 +2,22 @@ import { Col, Row } from 'antd'
 import 'antd/dist/antd.css'
 import * as React from 'react'
 import styled from 'styled-components'
-import TotalTime from '../components/Contents/Activity/TotalTime'
+import ActiveHours from '../components/contents/Activity/Hours'
+import TotalTime from '../components/contents/Activity/TotalTime'
+import ActiveWeekdays from '../components/contents/Activity/Weekdays'
+import ProductiveHours from '../components/contents/Productivity/Hours'
+import Score from '../components/contents/Productivity/Score'
+import ProductiveWeekdays from '../components/contents/Productivity/Weekdays'
+import Applications from '../components/contents/Charts/Applications'
+import Projects from '../components/contents/Charts/Projects'
+import Stacked from '../components/contents/Charts/Stacked'
 
 const Container = styled('div')`
   background-color: #eeeeee;
 `
 
 const Content = styled(Col)`
-  padding: 10;
+  padding: 10px;
   display: flex;
   justify-content: center;
 `
@@ -25,37 +33,37 @@ const OverviewPage: React.FC = () => {
                 <TotalTime/>
               </Content>
               <Content xs={8}>
-                ActiveWeekdays
+                <ActiveWeekdays/>
               </Content>
               <Content xs={8}>
-                ActiveHours
+                <ActiveHours/>
               </Content>
             </Col>
           </Row>
           <Row>
             <Col xs={24}>
               <Content xs={8}>
-                Score
+                <Score/>
               </Content>
               <Content xs={8}>
-                ProductiveWeekday
+                <ProductiveWeekdays/>
               </Content>
               <Content xs={8}>
-                ProductiveHours
+                <ProductiveHours/>
               </Content>
             </Col>
           </Row>
         </Col>
         <Content lg={12}>
-          Stackedbar
+          <Stacked/>
         </Content>
       </Row>
       <Row>
         <Content lg={12}>
-          Applications
+          <Applications/>
         </Content>
         <Content lg={12}>
-          Projects
+          <Projects/>
         </Content>
       </Row>
     </Container>
