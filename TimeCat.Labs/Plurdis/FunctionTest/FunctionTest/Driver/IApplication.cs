22ad2b1c1c94bae5daec99171fb9a6fb31d1149c
Plurdis <uutak2000@naver.com>
2019-10-12 18:45:27 +0900

    Add IApplication Interface

---
+++ TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Driver/IApplication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionTest.Driver
+{
+    public interface IApplication
+    {
+        int ProcessId { get; }
+
+        string Caption { get; }
+    }
+}
