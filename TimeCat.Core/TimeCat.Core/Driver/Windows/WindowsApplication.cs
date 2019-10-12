1346be1c28082677366e572f52e7bd9fbe00c268
Plurdis <uutak2000@naver.com>
2019-10-12 19:22:12 +0900

    Porting WindowsApplication/Driver to TimeCat.Core

---
+++ TimeCat.Core/TimeCat.Core/Driver/Windows/WindowsApplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace FunctionTest.Driver.Windows
+{
+    public class WindowsApplication : IApplication
+    {
+        public int ProcessId { get; set; }
+
+        public string Name { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Version { get; set; }
+    }
+}
