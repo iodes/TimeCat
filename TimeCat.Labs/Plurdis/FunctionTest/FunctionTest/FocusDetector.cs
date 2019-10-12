4574b579fa25065c7b12f304e80515b3069d7166
Plurdis <uutak2000@naver.com>
2019-10-12 18:21:39 +0900

    Before start FocusDetector add wait code to initalizing thread

--- TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/FocusDetector.cs
+++ TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/FocusDetector.cs
@@ -12,35 +12,54 @@ namespace FunctionTest
 
         static string lastTitle;
         static int fullCtr = 0;
-        static bool isInitalized = false;
+        static bool _running = false;
 
-        public static void InitalizeHook()
+        static ManualResetEvent _manualResetEvent;
+
+        static FocusDetector()
         {
-            if (!isInitalized)
+            _manualResetEvent = new ManualResetEvent(false);
+        }
+
+        public static void Start()
+        {
+            if (_running)
+                return;
+
+            _running = true;
+
+            Thread thread = new Thread(() =>
             {
-                Thread thread = new Thread(() =>
-                {
-                    User32.SetWinEventHook(User32.eventSystemForeground,
-                        User32.eventSystemForeground,
-                        IntPtr.Zero,
-                        dele,
-                        0,
-                        0,
-                        User32.winEventOutOfContext);
-                });
-                thread.SetApartmentState(ApartmentState.STA);
-                thread.Start();
-                
-                isInitalized = true;
+                User32.SetWinEventHook(User32.eventSystemForeground,
+                    User32.eventSystemForeground,
+                    IntPtr.Zero,
+                    dele,
+                    0,
+                    0,
+                    User32.winEventOutOfContext);
+
+                _manualResetEvent.WaitOne();
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+
+        public static void Stop()
+        {
+            if (_running)
+            {
+                _manualResetEvent.Set();
+                _running = false;
             }
         }
 
         private static void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
         {
-            Console.WriteLine($"'{lastTitle}' Window was selected while {fullCtr * 100}ms.");
             fullCtr = 0;
-            string title = User32.GetActiveWindowTitle();
-            Console.WriteLine($"Selected Window Changes to {title}");
+            string title = User32.GetWindowTitle(hwnd);
+
+            Console.WriteLine($"Selected Window Changes to {title} pid {User32.GetWindowThreadProcessId(hwnd)}");
 
             lastTitle = title;
         }
