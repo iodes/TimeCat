56af39501e1d7d286a569ebb6f3d51a0ead0c105
Eunjeong Park <ejisvip@gmail.com>
2019-10-13 02:55:44 +0900

    Delete and move unnecessary files

--- TimeCat.Labs/Ejolie/d3-timeline/src/index.js
+++ TimeCat.Labs/Ejolie/d3-timeline/src/index.js
@@ -3,11 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import ChartApp from './ChartApp';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<ChartApp />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
