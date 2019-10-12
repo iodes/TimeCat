10668ff4a3e118021c9cefed7d72f8509933c798
Plurdis <uutak2000@naver.com>
2019-10-12 18:29:48 +0900

    Add StateType from RpcActionType

---
+++ TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Common/StateType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionTest.Common
+{
+    enum StateType
+    {
+        Focus = 0,
+        Blur = 1,
+        Open = 2,
+        Close = 3,
+        Active = 4,
+        Idle = 5,
+    }
+}
