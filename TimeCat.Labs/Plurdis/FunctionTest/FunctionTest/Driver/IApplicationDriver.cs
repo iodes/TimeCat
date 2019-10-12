df60adf7efef09d553dd5ae2515a21abde082469
Plurdis <uutak2000@naver.com>
2019-10-12 18:45:51 +0900

    Add IApplicationDriver Interface

---
+++ TimeCat.Labs/Plurdis/FunctionTest/FunctionTest/Driver/IApplicationDriver.cs
@@ -0,0 +1,12 @@
+﻿using FunctionTest.EventArg;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionTest.Driver
+{
+    interface IApplicationDriver
+    {
+        event EventHandler<StateChangedEventArgs> StateChanged;
+    }
+}
