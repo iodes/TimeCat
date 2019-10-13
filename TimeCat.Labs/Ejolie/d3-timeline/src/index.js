2d29758068ace8ce7f9152d601f2080d46055a92
Eunjeong Park <ejisvip@gmail.com>
2019-10-13 16:32:39 +0900

    Implement responsive chart

--- TimeCat.Labs/Ejolie/d3-timeline/src/index.js
+++ TimeCat.Labs/Ejolie/d3-timeline/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/index.css';
 // import App from './App';
-import ChartApp from './ChartApp';
+// import App from './ChartApp';
+import App from './ResponsiveApp';
 
-ReactDOM.render(<ChartApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
