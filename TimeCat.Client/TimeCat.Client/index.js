cdf0f714297fe8400b1c1ab1a3ac31e6bd7fe0e3
TaeSang Cho <gsts007@gmail.com>
2019-10-19 01:59:15 +0900

    [Client] Add category grpc middleware

--- TimeCat.Client/TimeCat.Client/index.js
+++ TimeCat.Client/TimeCat.Client/index.js
@@ -1,38 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 
-let mainWindow;
-
-async function openMainWindow()
-{
-  if (mainWindow) return;
-
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-    titleBarStyle: 'hiddenInset',
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-
-  mainWindow.webContents.openDevTools();
-
-  await mainWindow.webContents.loadFile('frontend/index.html');
-}
-
-app.on('ready', openMainWindow);
-app.on('activate', openMainWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform === 'darwin') return;
-
-  app.quit();
-});
-
 const grpc = require('grpc');
 
 const { RpcCategoryServiceClient } = require('./proto/core/services/category_service_grpc_pb');
@@ -41,10 +8,12 @@ const { RpcDashboardServiceClient } = require('./proto/core/services/dashboard_s
 const { RpcDetailServiceClient } = require('./proto/core/services/detail_service_grpc_pb');
 const { RpcMainServiceClient } = require('./proto/core/services/main_service_grpc_pb');
 const { RpcReviewServiceClient } = require('./proto/core/services/review_service_grpc_pb');
-const { HealthCheckRequest } = require('./proto/core/services/common_service_pb');
-const { Timestamp } = require('./proto/google/protobuf/timestamp_pb');
 
-const { Empty } = require('./proto/core/common_pb');
+const { Empty, TimestampRange } = require('./proto/core/common_pb');
+const { InitializeRequest, HealthCheckRequest } = require('./proto/core/services/common_service_pb');
+const { CategoryCreateRequest, CategoryUpdateRequest, CategoryDeleteRequest } = require('./proto/core/services/category_service_pb');
+const { TotalTimeRequest, ApplicationRequest } = require('./proto/core/services/dashboard_service_pb');
+const { Timestamp } = require('./proto/google/protobuf/timestamp_pb');
 
 let category = new RpcCategoryServiceClient('localhost:37013', grpc.credentials.createInsecure());
 let common = new RpcCommonServiceClient('localhost:37013', grpc.credentials.createInsecure());
@@ -53,6 +22,25 @@ let detail = new RpcDetailServiceClient('localhost:37013', grpc.credentials.crea
 let main = new RpcMainServiceClient('localhost:37013', grpc.credentials.createInsecure());
 let review = new RpcReviewServiceClient('localhost:37013', grpc.credentials.createInsecure());
 
+const convertTimestamp = (date) => {
+  let timestamp = new Timestamp();
+  timestamp.fromDate(date);
+
+  return timestamp;
+};
+
+const convertTimestampRange = (range) => {
+  let timestampRange = new TimestampRange();
+
+  let start = convertTimestamp(range.start);
+  let end = convertTimestamp(range.end);
+
+  timestampRange.setStart(start);
+  timestampRange.setEnd(end);
+
+  return timestampRange;
+};
+
 let commands = {
   'category.GetCategories': (data, callback) => {
     let request = new Empty();
@@ -60,44 +48,42 @@ let commands = {
     category.getCategories(request, (err, response) => {
       if (err) return;
 
-      console.log(response);
-
-      callback(response);
+      callback(response.toObject());
     });
   },
   'category.CreateCategory': (data, callback) => {
+    let request = new CategoryCreateRequest();
+    request.setParentid(data.parentId);
+    request.setName(data.name);
+    request.setColor(data.color);
 
-  },
-  'category.UpdateCategory': (data, callback) => {
-
-  },
-  'category.DeleteCategory': (data, callback) => {
-
-  },
-  'common.Initialize': (data, callback) => {
-
-  },
-  'common.HealthCheck': (data, callback) => {
-    let request = new HealthCheckRequest();
-    let currentTime = new Timestamp();
-    let responseTime = new Timestamp();
-
-    currentTime.fromDate(new Date(data.currentTime));
-    responseTime.fromDate(new Date(data.responseTime));
-
-    request.setCurrenttime(currentTime);
-    request.setResponsetime(responseTime);
-
-    common.healthCheck(request, (err, response) => {
+    category.createCategory(request, (err, response) => {
       if (err) return;
 
-      let currentTime = response.getCurrenttime().toDate();
-      let requestTime = response.getRequesttime().toDate();
+      callback(response.toObject());
+    });
+  },
+  'category.UpdateCategory': (data, callback) => {
+    let request = new CategoryUpdateRequest();
+    request.setId(data.id);
+    request.setName(data.name);
+    request.setColor(data.color);
+    request.setParentid(data.parentId);
 
-      callback({
-        currentTime,
-        requestTime,
-      });
+    category.updateCategory(request, (err, response) => {
+      if (err) return;
+
+      callback(response.toObject());
+    });
+  },
+  'category.DeleteCategory': (data, callback) => {
+    let request = new CategoryDeleteRequest();
+    request.setId(data.id);
+
+    category.deleteCategory(request, (err, response) => {
+      if (err) return;
+
+      callback(response.toObject());
     });
   },
   'dashboard.GetTotalTime': (data, callback) => {
@@ -123,4 +109,42 @@ ipcMain.on('grpc', (event, command, request) => {
   commands[command](request, (response) => {
     event.reply('grpc', command, response);
   });
+});
+
+let mainWindow;
+async function openMainWindow()
+{
+  if (mainWindow) return;
+
+  let request = new InitializeRequest();
+  request.setAppversion('1.0.0');
+
+  common.initialize(request, (err, response) => {
+    if (err) return app.quit();
+
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+      },
+      titleBarStyle: 'hiddenInset',
+    });
+
+    mainWindow.on('closed', () => {
+      mainWindow = null;
+    });
+
+    mainWindow.webContents.openDevTools();
+    mainWindow.webContents.loadFile('frontend/index.html');
+  });
+}
+
+app.on('ready', openMainWindow);
+app.on('activate', openMainWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform === 'darwin') return;
+
+  app.quit();
 });
