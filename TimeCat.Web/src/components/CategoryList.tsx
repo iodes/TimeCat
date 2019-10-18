1f00154c781c1c6eb6021be3d67a219045b6b505
김형규 <k7120792@gmail.com>
2019-10-19 00:04:11 +0900

    Add category component

--- TimeCat.Web/src/components/CategoryList.tsx
+++ TimeCat.Web/src/components/CategoryList.tsx
@@ -1,13 +1,167 @@
 import * as React from 'react'
+import CategoryItem from './CategoryItem'
+import { AntTreeNodeSelectedEvent } from 'antd/es/tree'
+import { Tree } from 'antd'
+import { AntTreeNodeDropEvent } from 'antd/es/tree/Tree'
+import { AntTreeNodeDragEnterEvent } from 'antd/lib/tree/Tree'
 
-export class CategoryList extends React.Component {
+const { TreeNode, DirectoryTree } = Tree
+
+interface IGData {
+	key: string
+	title: string
+	children: IGData[]
+}
+
+interface IProps {
+	category: number[]
+}
+
+interface IState {
+	gData: IGData[]
+	expandedKeys: string[]
+}
+
+const x = 3
+const y = 2
+const z = 1
+const gData: IGData[] = []
+
+// tslint:disable-next-line:variable-name
+const generateData = (_level: number, _preKey: string, _tns: IGData[]) => {
+	const preKey = _preKey || '0'
+	const tns = _tns || gData
+
+	const children = []
+	for (let i = 0; i < x; i++) {
+		const key = `${preKey}-${i}`
+		tns.push({ title: key, key, children: [] })
+		if (i < y) {
+			children.push(key)
+		}
+	}
+	if (_level < 0) {
+		return tns
+	}
+	const level = _level - 1
+	children.forEach((key, index) => {
+		tns[index].children = []
+		return generateData(level, key, tns[index].children)
+	})
+}
+
+export class CategoryList extends React.Component<{}, IState> {
+	public state = {
+		gData,
+		expandedKeys: ['0-0', '0-0-0', '0-0-0-0'],
+	}
+
 	constructor(props: {}) {
 		super(props)
 	}
 
+	public onDragEnter = (info: AntTreeNodeDragEnterEvent) => {
+		// tslint:disable-next-line:no-console
+		console.log(info)
+		// expandedKeys 需要受控时设置
+		// this.setState({
+		//   expandedKeys: info.expandedKeys,
+		// });
+	}
+
+	public onDrop = (info: AntTreeNodeDropEvent) => {
+		// tslint:disable-next-line:no-console
+		console.log(info)
+		const dropKey = info.node.props.eventKey || ''
+		const dragKey = info.dragNode.props.eventKey || ''
+		const dropPos = info.node.props.pos.split('-')
+		const dropPosition = info.dropPosition - Number(dropPos[dropPos.length - 1])
+
+		// tslint:disable-next-line:no-shadowed-variable
+		const loop = (data: IGData[], key: string, callback: any) => {
+			data.forEach((item, index, arr) => {
+				if (item.key === key) {
+					return callback(item, index, arr)
+				}
+				if (item.children) {
+					return loop(item.children, key, callback)
+				}
+			})
+		}
+		const data = [...this.state.gData]
+
+		// Find dragObject
+		let dragObj: any
+		loop(data, dragKey, (item: IGData, index: number, arr: IGData[]) => {
+			arr.splice(index, 1)
+			dragObj = item
+		})
+
+		if (!info.dropToGap) {
+			// Drop on the content
+			loop(data, dropKey, (item: IGData) => {
+				item.children = item.children || []
+				// where to insert 示例添加到尾部，可以是随意位置
+				item.children.push(dragObj)
+			})
+		} else if (
+			(info.node.props.children || []).length > 0 && // Has children
+			info.node.props.expanded && // Is expanded
+			dropPosition === 1 // On the bottom gap
+		) {
+			loop(data, dropKey, (item: IGData) => {
+				item.children = item.children || []
+				// where to insert 示例添加到尾部，可以是随意位置
+				item.children.unshift(dragObj)
+			})
+		} else {
+			let ar: any
+			let i: number = 0
+			loop(data, dropKey, (item: IGData, index: number, arr: IGData[]) => {
+				ar = arr
+				i = index
+			})
+			if (dropPosition === -1) {
+				ar.splice(i, 0, dragObj)
+			} else {
+				ar.splice(i + 1, 0, dragObj)
+			}
+		}
+
+		this.setState({
+			gData: data,
+		})
+	}
+
 	public render() {
+		const loop: (data: IGData[]) => JSX.Element[] = (data) =>
+			data.map((item: IGData) => {
+				if (item.children && item.children.length) {
+					return (
+						<TreeNode key={item.key} title={item.title}>
+							{loop(item.children)}
+						</TreeNode>
+					)
+				}
+				return <TreeNode key={item.key} title={item.title} />
+			})
+
 		return (
-			<h2>Hello, CategoryList Component</h2>
+			<div>
+				<h4>projects</h4>
+				<div className="category-wrapper">
+					<Tree
+						className="draggable-tree"
+						defaultExpandedKeys={this.state.expandedKeys}
+						draggable
+						blockNode
+						onDragEnter={this.onDragEnter}
+						onDrop={this.onDrop}
+					>
+						{loop(this.state.gData)}
+					</Tree>
+				</div>
+			</div>
 		)
 	}
 }
--- TimeCat.Web/src/pages/DetailsPage.tsx
+++ TimeCat.Web/src/pages/DetailsPage.tsx
@@ -1,13 +1,96 @@
+import { Button, Layout, Table } from 'antd'
+import { ColumnProps } from 'antd/es/table'
 import * as React from 'react'
+import CategoryList from '../components/CategoryList'
+import TimeLine from '../components/Timeline'
 
-export class DetailsPage extends React.Component {
-	constructor(props: {}) {
+import { ITimeline } from '../types/timeline'
+import './DetailsPage.css'
+
+interface IProps {
+	selectedProject: string
+}
+
+interface IState {
+	selectedProject: string
+	projectRunningTime: string
+}
+
+export class DetailsPage extends React.Component<IProps, IState> {
+	constructor(props: IProps) {
 		super(props)
+		this.state = {
+			selectedProject: 'unassigned',
+			projectRunningTime: '9m 50s',
+		}
 	}
 
 	public render() {
+		const dataSource: ITimeline[] = [
+			{
+				key: 1,
+				duration: 8,
+				timespan: '21/09/2019..23123123123.',
+				detail: 'blabla1',
+			},
+			{
+				key: 2,
+				duration: 10,
+				timespan: '21/09/2019...',
+				detail: 'blabla2',
+			},
+			{
+				key: 3,
+				duration: 7,
+				timespan: '21/09/2019...',
+				detail: 'blabla3',
+			},
+		]
+		function sorter(a: ITimeline, b: ITimeline) {
+			return a.duration - b.duration
+		}
+		const columns: ColumnProps<ITimeline>[] = [
+			{
+				dataIndex: 'duration',
+				key: 'duration',
+				sorter,
+				width: 50,
+				title: 'Duration',
+			},
+			{
+				dataIndex: 'timespan',
+				key: 'timespan',
+				width: 150,
+				title: 'Timespan',
+			},
+			{
+				dataIndex: 'detail',
+				key: 'detail',
+				title: 'Details',
+			},
+		]
+		const { selectedProject, projectRunningTime } = this.state
 		return (
-			<h1>Hello, Details Page</h1>
+			<Layout className="section">
+				<Layout.Header className="header">
+					<TimeLine />
+				</Layout.Header>
+				<Layout>
+					<Layout.Sider className="sider">
+						<CategoryList />
+					</Layout.Sider>
+					<Layout className="detail-content">
+						<h2>{selectedProject}: {projectRunningTime}</h2>
+						<div className="content">
+							<div className="detail-page-header">
+								<span>Details</span>
+								<Button>time</Button>
+							</div>
+							<Table dataSource={dataSource} columns={columns} />
+						</div>
+					</Layout>
+				</Layout>
+			</Layout>
 		)
 	}
 }
