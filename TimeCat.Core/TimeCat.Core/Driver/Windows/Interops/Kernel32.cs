38f05b1b6bced673fb952500298ca843a00b63fd
SteaI <godhoop@nate.com>
2019-10-19 18:15:14 +0900

    impl window driver

---
+++ TimeCat.Core/TimeCat.Core/Driver/Windows/Interops/Kernel32.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TimeCat.Core.Driver.Windows.Interops
+{
+    internal static class Kernel32
+    {
+        [DllImport(ExternDll.Kernel32, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport(ExternDll.Kernel32)]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport(ExternDll.Kernel32, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetExitCodeProcess(IntPtr hProcess, out uint lpExitCode);
+
+    }
+}
