701b063671560b9ccfb7dbe8f02bbd9e90b3b46d
Plurdis <uutak2000@naver.com>
2019-10-19 13:35:11 +0900

    Remove ProcessId & Provide WindowsApplication.FromPath Function

---
+++ TimeCat.Core/TimeCat.Core/Driver/Windows/Interop/Shell32.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using TimeCat.Core.Interop;
+
+namespace TimeCat.Core.Driver.Windows.Interop
+{
+    static class Shell32
+    {
+        [DllImport(ExternDll.Shell32)]
+        public static extern IntPtr ExtractIconEx(string sFile, int iIndex, out IntPtr piLargeVersion, out IntPtr piSmallVersion, int amountIcons);
+    }
+}
