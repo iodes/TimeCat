e12599019706bf47e7b2f74b8712c35934120fc2
Plurdis <uutak2000@naver.com>
2019-10-19 13:35:11 +0900

    Remove ProcessId & Provide WindowsApplication.FromPath Function

---
+++ TimeCat.Core/TimeCat.Core/Driver/Windows/Common/EnvironmentSupport.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace TimeCat.Core.Driver.Windows.Common
+{
+    public static class EnvironmentSupport
+    {
+        const string cacheDir = "Cache";
+
+        static readonly string _storage = $@"{System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData)}\TimeCat";
+
+        public static string Cache
+        {
+            get
+            {
+                var combine = Path.Combine(Storage, cacheDir);
+
+                if (!Directory.Exists(combine))
+                    Directory.CreateDirectory(combine);
+
+                return combine;
+            }
+        }
+
+        public static string Storage
+        {
+            get
+            {
+                if (!Directory.Exists(_storage))
+                    Directory.CreateDirectory(_storage);
+
+                return _storage;
+            }
+        }
+    }
+}
