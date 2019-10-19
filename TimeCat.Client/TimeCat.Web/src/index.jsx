e2dd738f3b85452c74a46e6771185e3eb1abb2c2
TaeSang Cho <gsts007@gmail.com>
2019-10-19 12:40:10 +0900

    Change extension tsx, ts to jsx, js

--- TimeCat.Client/TimeCat.Web/src/index.jsx
+++ TimeCat.Client/TimeCat.Web/src/index.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import store from './stores'
+import store from './stores/index'
 
 import App from './App'
 
