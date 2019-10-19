8730e3d2d8c9a40905016ec57db6cff40de2a36c
SteaI <godhoop@nate.com>
2019-10-19 18:55:40 +0900

    Fix process info

--- TimeCat.Core/TimeCat.Core/Driver/Windows/Interops/Kernel32.cs
+++ TimeCat.Core/TimeCat.Core/Driver/Windows/Interops/Kernel32.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace TimeCat.Core.Driver.Windows.Interops
 {
@@ -15,5 +16,14 @@ namespace TimeCat.Core.Driver.Windows.Interops
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool GetExitCodeProcess(IntPtr hProcess, out uint lpExitCode);
 
+        [DllImport(ExternDll.Kernel32)]
+        public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("psapi.dll")]
+        public static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, [In] [MarshalAs(UnmanagedType.U4)] int nSize);
+
+        [DllImport(ExternDll.Kernel32, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
     }
 }
